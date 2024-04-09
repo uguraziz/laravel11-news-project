c952f82e3b2992bcbf149c7fb65bdb652d5b8fda
Uğur ALTUN <aziz_altun93@hotmail.com>
2024-04-09 09:44:09 +0300

    SoftDelete and RelationShips test example

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\News;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class TestController extends Controller
+{
+    public function test_metodu()
+    {
+        // $user = $this->create_user();
+
+        // $categori_modeli = Category::create([
+        //     'name' => 'Test Kategori',
+        //     'slug' => 'test-kategori '. rand(1, 1000),
+        //     'image' => 'test.jpg',
+        // ]);
+
+        // News::create([
+        //     'title' => 'Test Haber',
+        //     'slug' => 'test-haber '.rand(1, 1000),
+        //     'content' => 'Test Haber İçeriği',
+        //     'image' => 'test.jpg',
+        //     'category_id' => $categori_modeli->id,
+        //     'user_id' => $user->id,
+        //     'is_active' => 1,
+        // ]);
+
+        // softDelete
+        // $haber = News::find(1)->delete();
+
+        // all news
+        // $haber = News::with('category', 'user')
+        // ->withTrashed()
+        // ->get();
+
+        $haber = News::with('category', 'user')->get();
+
+        dd($haber);
+    }
+
+    public function create_user(): User
+    {
+        return User::firstOrCreate([
+            'name' => 'Test User'
+        ], [
+            'email' => 'aziz_altun93@hotmail.com',
+            'password' => Hash::make('123'),
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
 
-Route::get('/info', function () {
+Route::get('info', function () {
     return view('info');
 });
+
+Route::get('test', [TestController::class, 'test_metodu']);
+
